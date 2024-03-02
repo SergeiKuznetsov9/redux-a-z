f32883d10395f2ba0fec237939651abdafed166b
Sergey Kuznetsov <Kuznetsoff9@yandex.ru>
2024-03-02 15:58:49 +0300

    Добавлен счетчик на тулките

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,8 +1,10 @@
+import { Counter } from "../../features/counter/counter";
 import { UsersList } from "../usersList/usersList";
 import cls from "./main.module.css";
 
 export const Main = () => (
   <div className={cls.Main}>
+    <Counter />
     <UsersList />{" "}
   </div>
 );
